255a140be9b3523f2240fb0eaea0b1ae25331258
aarichirag <patelarati1993@gmail.com>
2020-12-18 16:23:27 +0530

    even number between 1 to 100

--- even_odd number.py
+++ even_odd number.py
@@ -1,5 +1,9 @@
-no=int(input("Enter Number:"))
-if no % 2==0:
-    print("Even number")
-else:
-    print("odd number")
+def is_even(no):
+    if no % 2==0:
+        return True
+    else:
+        return False
+
+for i in range(101):
+    if is_even(i)==True:
+        print(i)
