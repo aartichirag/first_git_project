6437769c9254f4bee4da7f5b056305f9c328a33c
Mrunal Shah <shahmrunal777@gmail.com>
2020-12-18 16:55:57 +0530

    Edited the product file and now the price is also displayed by taking the product's rate and quantity from the user

--- Product_demo.py
+++ Product_demo.py
@@ -1,7 +1,22 @@
+
+
 product_id=int(input("Enter Product id:"))
+
 product_name =input("Enter Product Name:")
-product_price=int(input("Enter Product price:"))
+
+product_quantity=int(input("ENter the product quantity:"))
+
+product_rate=int(input("Enter the product rate:"))
+
+
+
+
+
+
 print("====================================")
 print("product_id:",product_id)
 print("product_name:",product_name)
-print("product_price:",product_price)
+print("product_quantity:",product_quantity)
+print("product_rate:",product_rate)
+
+print("Product_prize:",product_rate*product_quantity)
